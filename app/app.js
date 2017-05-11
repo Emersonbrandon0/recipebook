c5f24e5f8f5258562ce5c0666f18dee44b32bf05
Brandon Emerson <emersonbrandon0@gmail.com>
2017-05-11 00:00:01 -0400

    Updated forms and logic

--- app/app.js
+++ app/app.js
@@ -1,15 +1,31 @@
 var recipeApp = angular.module('recipeApp',[]);
 
 recipeApp.controller('recipeFormController',['$scope',function($scope){
-  console.log(1232323)
   $scope.ingredients=[];
   $scope.addIngredient=function(ingredient){
+    if(ingredient===undefined){
+      return;
+    }
     $scope.ingredients.push(
       {
         "ingredient":ingredient
       }
     );
-    console.log($scope.ingredients);
     $scope.ingredient='';
+    $scope.ingredient=undefined;
   };
+
+  $scope.directions=[];
+  $scope.addDirection=function(direction){
+    if(direction===undefined){
+      return;
+    }
+    $scope.directions.push(
+      {
+        "direction":direction
+      }
+    );
+    $scope.direction='';
+    $scope.direction=undefined;
+  }
 }]);
