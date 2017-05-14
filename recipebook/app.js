189f8efb5e59111fbb0317cba7a858145a8dfc14
Brandon Emerson <emersonbrandon0@gmail.com>
2017-05-14 15:43:30 -0400

    Implemented register system

--- recipebook/app.js
+++ recipebook/app.js
@@ -2,8 +2,18 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var expressValidator=require('express-validator')
 var cookieParser = require('cookie-parser');
+var session=require('express-session');
+var passport=require('passport');
+var localStrategy=require('passport-local').Strategy;
+var multer=require('multer');
+var flash=require('connect-flash');
+var mongo=require('mongodb');
+var mongoose=require('mongoose');
+var db=mongoose.connection;
 var bodyParser = require('body-parser');
+var bcrypt = require('bcryptjs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,6 +24,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Handle file uploads
+var upload = multer({ dest: 'uploads/' });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,8 +33,43 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Handle express sessions
+app.use(session({
+  secret:'secret',
+  saveUninitialized:true,
+  resave:true
+}));
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.get('*',function(req,res,next){
   res.locals.user=req.user || null;
--- recipebook/routes/index.js
+++ recipebook/routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
+var session=require('express-session');
 var path = require('path');
 var router = express.Router();
+var User = require('../models/user');
+var multer=require('multer');
+var passport=require('passport');
+var LocalStrategy=require('passport-local').Strategy;
+
+router.use(session({ secret: 'my super secret',name: 'my-id', resave: false, saveUninitialized: false }));
+router.use(passport.initialize());
+router.use(passport.session());
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,6 +24,101 @@ router.get('/register',function(req, res, next){
 router.get('/login',function(req,res,next){
 	res.render('login',{title:'Login'});
 });
+
+router.post('/register', multer({ dest: './uploads/'}).single('upl'), function(req, res, next) {
+	//Get form values
+	console.log(req.body);
+	var name=req.body.name;
+	var email=req.body.email;
+	var username=req.body.username;
+	var password=req.body.password;
+	var password2=req.body.password2;
+
+	//Check for image field
+	if(req.file){
+		console.log('Uploading file');
+
+		//File info
+		var profileImageOriginalName=req.files.profileImage.originalname;
+		var profileImageName=req.files.profileImage.name;
+		var profileImageMime=req.files.profileImage.mimeType;
+		var profileImagePath=req.files.profileImage.path;
+		var profileImageExtension=req.files.profileImage.extension;
+		var profileImageSize=req.files.profileImage.size;
+	} else{
+		//Set default image
+		var profileImageName='noImage.jpg';
+	}
+
+	//Form validation
+	req.checkBody('name','Name field is required').notEmpty();
+	req.checkBody('email','Email is required').isEmail();
+	req.checkBody('username','Username is required').notEmpty();
+	req.checkBody('password','Password is required').notEmpty();
+	req.checkBody('password2','Passwords do not match').equals(password);
+
+	var errors=req.validationErrors()
+	console.log(errors);
+	if(errors){
+		res.render('register',{
+			errors:errors,
+			name:name,
+			email:email,
+			username:username,
+			password:password,
+			password2:password2
+		});
+	} else {
+		var newUser = new User({
+			name:name,
+			email:email,
+			username:username,
+			password:password,
+			profileImage:profileImageName
+		});
+
+		//Create user
+		User.createUser(newUser, function(error,user){
+			if(error) throw error;
+			console.log(user);
+		});
+
+		//Success message
+		req.flash('success','You are now registered and may log in');
+		res.location('/');
+		res.redirect('/');
+	}	
+});
+
+passport.use(new LocalStrategy(
+function(username, password, done) {
+     User.getUserByUsername(username, function (err, user) {
+          if (err) { return done(err); }
+          if (!user) {
+               return done(null, false, { message: 'Incorrect username.' });
+          }
+          User.comparePassword(password,user.password,function(err,isMatch){
+          	if (err) {return done(err);}
+          	if (isMatch) {
+          		return done(null,user);
+          	} else{
+          		console.log('Invalid password');
+          		return done(null,false, {message: 'Invalid password.'});
+          	}
+          });
+       });
+    }
+
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 
 
 module.exports = router;
